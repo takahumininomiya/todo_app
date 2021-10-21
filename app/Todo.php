76d452b28b4112b4abf2392b019b199849899f19
EC2 Default User <ec2-user@ip-172-31-83-30.ec2.internal>
2021-10-21 18:17:08 +0000

    imp; relation

--- app/Todo.php
+++ app/Todo.php
@@ -17,4 +17,8 @@ class Todo extends Model
     {
        return $this->belongsTo('App\Goal');
    }
+   public function tags()
+    {
+        return $this->belongsToMany('App\Tag');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -44,4 +44,8 @@ class User extends Authenticatable
     {
         return $this->hasMany('App\Todo');
     }
+        public function tags()
+    {
+        return $this->hasMany('App\Tag');
+    }
 }
