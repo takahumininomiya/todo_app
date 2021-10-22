fa3480d36f86f420221797b34987b6e8733c650f
EC2 Default User <ec2-user@ip-172-31-83-30.ec2.internal>
2021-10-22 13:40:32 +0000

    npm run dev

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Todo;
 + use App\Goal;
+use App\Tag;
 use Illuminate\Http\Request;
 + use Illuminate\Support\Facades\Auth;
 
@@ -16,7 +17,7 @@ class TodoController extends Controller
      */
     public function index(Request $request, Goal $goal)
     {
-        $todos = $goal->todos()->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
+        $todos = $goal->todos()->with('tags')->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
 +        return response()->json($todos);
     }
     /**
@@ -34,7 +35,7 @@ class TodoController extends Controller
 +        $todo->position = request('position');
 +        $todo->done = false;
 +        $todo->save();
-+        $todos = $goal->todos()->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
+$todos = $goal->todos()->with('tags')->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
 +        return response()->json($todos);
     }
     **
@@ -52,8 +53,7 @@ class TodoController extends Controller
 +        $todo->position = request('position');
 +        $todo->done = (bool) request('done');
 +        $todo->save();
-+
-+        $todos = $goal->todos()->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
++$todos = $goal->todos()->with('tags')->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
 +        return response()->json($todos);
     }
     /**
@@ -65,7 +65,7 @@ class TodoController extends Controller
      public function destroy(Request $request, Goal $goal,Todo $todo)
     {
         $todo->delete();
-+        $todos = $goal->todos()->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
+    $todos = $goal->todos()->with('tags')->orderBy('done', 'asc')->orderBy('position', 'asc')->get();    
 +        return response()->json($todos);
     }
     public function sort(Request $request, Goal $goal, Todo $todo)
@@ -80,9 +80,26 @@ class TodoController extends Controller
         } else {
             $todo->moveAfter($exchangeTodo);
         }
-
-        $todos = $goal->todos()->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
+$todos = $goal->todos()->with('tags')->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
+        
 
         return response()->json($todos);
     }
+    public function addTag(Request $request, Goal $goal, Todo $todo, Tag $tag)
++    {
++        $todo->tags()->attach($tag->id);
++
++        $todos = $goal->todos()->with('tags')->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
++
++        return response()->json($todos);
++    }
+
++    public function removeTag(Request $request, Goal $goal, Todo $todo, Tag $tag)
++    {
++        $todo->tags()->detach($tag->id);
++
++        $todos = $goal->todos()->with('tags')->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
++
++        return response()->json($todos);
++    }
 }
